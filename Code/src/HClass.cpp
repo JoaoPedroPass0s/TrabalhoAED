531c63218b134f107ca88167091f32a905264604
JoaoPedroPass0s <up202108833@up.pt>
2022-11-01 00:12:27 +0000

    Bug fix

--- Code/src/HClass.cpp
+++ Code/src/HClass.cpp
@@ -5,4 +5,4 @@ string HClass::getClass() const {return class_; }
 string HClass::getUc() const {return uc_; }
 list<Slot> HClass::getClassUcHour() const {return classUcHour_; }
 
-HClass::HClass(string uc, string classes, string classUcHour): uc_(uc), class_(classes), classUcHour_(classUcHour) {}
+HClass::HClass(string uc, string classes, list<Slot> classUcHour): uc_(uc), class_(classes), classUcHour_(classUcHour) {}
